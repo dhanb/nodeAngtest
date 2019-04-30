6ed9c93e113812620996b0e09a76d0d3ed9440d8
Danushka AKG <dattanak@ryerson.ca>
2019-04-30 14:54:43 -0400

    +

---
+++ l04/app.js
@@ -0,0 +1,8 @@
+(function() {
+  "use strict";
+  x = "hello";
+  angular
+    .module("l04", [])
+
+    .controller("L04", function() {});
+})();
---
+++ l04/index.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html ng-app="l04">
+
+<head>
+	<meta charset="utf-8">
+	<script src="angular.min.js"></script>
+	<script src="app.js"></script>
+
+	<title>L04</title>
+</head>
+
+
+<body>
+	<div ng-controller="L04">
+
+
+	</div>
+</body>
+
+
+</html>
